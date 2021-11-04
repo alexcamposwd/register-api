9d01c35eaafdf037162c8de80134cf5022b4eca6
Alex Campos <alexfilhotes@hotmail.com>
2021-11-04 16:04:34 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,247 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="navbar">
+      <h1 class="title">List of Useful API's</h1>
+    </div>
+    <div class="main">
+      <div class="card-form">
+        <div class="card-body">
+          <form @submit.prevent="sendRegister">
+            <div class="form-group">
+              <input type="text" 
+                placeholder="Name API" 
+                class="control"
+                v-model="register.name"
+              >
+            </div>
+            <div class="form-group">
+              <textarea type="text" 
+                placeholder="Description" 
+                class="control" rows="2" 
+                style="resize: none;" 
+                v-model="register.description"
+              ></textarea>
+            </div>
+            <div class="form-group double-inpt">
+              <input type="text" 
+                placeholder="Category" 
+                class="control"
+                v-model="register.category"
+              >
+              <input type="text" 
+                placeholder="Authentication" 
+                class="control"
+                v-model="register.authentication"
+              >
+            </div>
+            <div class="form-group">
+              <input type="text" 
+                placeholder="URL" 
+                class="control"
+                v-model="register.url"
+              >
+            </div>
+            <div class="btn-group">
+              <template v-if="edit === false">
+                <button class="btn-primary">
+                  Save
+                </button>                  
+             </template>
+             <template v-else> 
+                <button class="btn-primary">
+                  Update
+                </button>                  
+              </template>
+            </div>
+          </form>
+        </div>
+      </div>
+      <List 
+        @update-register= "updateRegister"
+        @delete-register= "deleteRegister"
+        :registers= "registers"
+        :edit= "edit"
+        :editRegister= "editRegister"
+        :register= "register"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
+import api from './services/api'
+
+class API {
+  constructor(name, description, category, authentication, url) {
+    this.name = name
+    this.description = description
+    this.category = category
+    this.authentication = authentication
+    this.url = url
+  }
+}
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    List
+  },
+
+  data() {
+    return {
+      register: new API(),
+      registers: [],
+      edit: false,
+      editRegister: '',
+    }
+  },
+
+  created() {
+    this.getRegister()
+  },
+
+  methods: {
+    
+    async getRegister() {
+      await api.get(`/register`)
+        .then((res) => { this.registers = res.data })
+    },
+
+    async sendRegister() {
+      if(this.edit === false) {
+        const response = await api.post('/register', this.register)
+        this.register = response.data 
+        this.getRegister()
+      } else {
+        const response = await api.put(`/register/`+ this.editRegister, this.register)
+        this.register = response 
+        this.getRegister()
+        this.edit = false
+      }
+      this.register = new API()
+    },
+  
+    async deleteRegister(id) {
+      const response = await api.delete(`/register/` + id)
+      this.register = response.data
+      this.getRegister()
+    },
+
+    async updateRegister(id) {
+      await api.get(`/register/` + id)
+      .then((res) => { 
+        this.register = res.data
+        this.editRegister = id
+        this.edit = true
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  width: 100%;
+  top: 0;
+  left: 0;
 }
+
+.navbar {
+  width: 100%;
+  height: 80px;
+  padding: 5px;
+  margin-top: 20px;
+}
+
+.title {
+  font-family: var(--font-family);
+  font-size: 35px;
+  font-weight: 600;
+  font-style: italic;
+  color: var(--color-41);
+  text-align: center;
+  margin-top: 5px;
+  text-transform: uppercase;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+  
+.card-form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-body {
+  width: 90%;
+  padding: 5px;
+  border: 1px solid var(--color-14);
+  border-radius: 3px;
+  margin-bottom: 10px;
+}
+
+.form-group {
+  width: 100%;
+  padding: 3px;
+}
+
+.double-inpt {
+  display: flex;
+}
+
+#app > div > div.main > div.card-form > div > form > div.form-group.double-inpt > input:nth-child(2) {
+  margin-left: 7px;
+}
+
+#app > div > div.main > div.card-form > div > form > div:nth-child(2) {
+  margin-bottom: -6px;
+}
+
+.control {
+  width: 100%;
+  font-family: var(--font-family);
+  font-size: 16px;
+  font-weight: 300;
+  padding: 3px;
+  padding-left: 7px;
+  border: 1px solid var(--color-14);
+  border-radius: 3px;
+  outline: none;
+}
+
+.btn-group {
+  display: flex;
+  background: #fff;
+  margin: 3px;
+  justify-content: center;
+}
+
+.btn-primary {
+  width: 300px;
+  height: 30px;
+  display: inline-block;
+  font-family: var(--font-family);
+  font-weight: 400;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  font-size: 17px;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+  cursor: pointer;
+}
+ 
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="card-list">
+    <div class="card-table">
+      <table>
+        <thead>
+          <tr>
+            <th id="name">Name API</th>
+            <th id="description">Description</th>
+            <th id="category">Category</th>
+            <th id="authentication">Auth</th>
+            <th id="url">URL</th>
+            <th id="edit">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item of registers" :key="item.id">
+            <td id="name">{{ item.name }}</td>
+            <td id="description">{{ item.description }}</td>
+            <td id="category">{{ item.category }}</td>
+            <td id="authentication">{{ item.authentication }}</td>
+            <td id="url" ><a  :href= "`${item.url}`" >{{ item.url }}</a></td>
+            <td id="edit">
+              <button class="btn-trash">
+                <img 
+                src="../assets/icon_trash.png" 
+                alt="icon-trash" 
+                class="icon-btn-trash"
+                @click="$emit('deleteRegister', item._id)"
+                >
+              </button>
+              <button class="btn-pencil">
+                <img 
+                src="../assets/icon_pencil.png" 
+                alt="icon-pencil" 
+                class="icon-btn-pencil"
+                @click="$emit('updateRegister', item._id)"
+                >
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'List',
+  emits:['updateRegister', 'deleteRegister'],
+
+  props: {
+    register: { type: Object },
+    registers: { type: Object },
+    edit: { type: Boolean },
+    editRegister: { type: String },
+  },
+}
+</script>
+
+<style scoped>
+.app {
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+
+.card-list {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-table {
+  width: 90%;
+  min-width: 350px;
+  padding: 5px;
+  border: 1px solid var(--color-14);
+  border-radius: 3px;
+}
+
+.card-table > table {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-width: 350px;
+  padding: 5px;
+  border: 1px solid var(--color-14);
+  justify-content: center;
+  align-items: center;
+}
+
+.card-table > table > thead,
+.card-table > table > tbody {
+  width: 100%;
+  margin-bottom: 5px;
+}
+
+.card-table > table > thead > tr,
+.card-table > table > tbody > tr {
+  display: flex;
+  width: 100%;
+}
+
+.card-table > table > thead > tr > th {
+  font-family: var(--font-family);
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: 3px;
+  color: var(--color-13);
+  text-align: center;
+  text-transform: uppercase;
+  border: 1px solid var(--color-14);
+}
+
+.card-table > table > tbody > tr > td {
+  font-size: 15px;
+  font-weight: 300;
+  line-height: 20px;
+  color: var(--color-12);
+  text-align: center;
+  margin: auto;
+  border-right: 1px solid var(--color-14);
+}
+
+.card-table > table > thead > tr > #name,
+.card-table > table > tbody > tr > #name {
+  width: 15%;
+}
+.card-table > table > thead > tr > #description,
+.card-table > table > tbody > tr > #description {
+  width: 30%;
+}
+.card-table > table > thead > tr > #category,
+.card-table > table > tbody > tr > #category {
+  width: 13%;
+}
+.card-table > table > thead > tr > #authentication,
+.card-table > table > tbody > tr > #authentication {
+  width: 7%;
+}
+.card-table > table > thead > tr > #url,
+.card-table > table > tbody > tr > #url {
+  width: 25%;
+}
+.card-table > table > thead > tr > #edit,
+.card-table > table > tbody > tr > #edit {
+  width: 10%;
+  min-width: 80px;
+}
+
+.btn-trash {
+  border: none;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  background: var(--color-42);
+  cursor: pointer;
+}
+
+.btn-pencil {
+  border: none;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  background: var(--color-35);
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.icon-btn-trash,
+.icon-btn-pencil {
+  width: 17px;
+  height: 15px;
+  margin: 5px;
+  filter: invert(1);
+}
+</style>
